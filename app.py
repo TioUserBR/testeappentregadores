f25ec9a40f5032bf45013c7d902b53928cbcf9ad
TioUserBR <tteulina@gmail.com>
2026-01-21 19:45:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -167,4 +167,6 @@ def apagar(id):
 # ---------------- MAIN ----------------
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
+
